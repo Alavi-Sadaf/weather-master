15757fba097d1e0b5d122a4e11031150a94a52c1
alavi al sadaf <alavialmahis@gmail.com>
2022-10-25 13:43:51 +0600

    Weather app done

---
+++ app.js
@@ -0,0 +1,20 @@
+const api_key = `e5a9bd879c43d7f71321a4eb51070211`;
+const submitBtn = document.getElementById('submit-btn');
+submitBtn.addEventListener('click', function () {
+  const inputBtn = document.getElementById('input-btn').value;
+
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputBtn}&appid=${api_key}&units=metric`)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    const cityName = data.name;
+    const temp = data.main.temp;
+    // const tempToCel = temp - 273.15;
+    const description = data.weather[0].description;
+
+    document.getElementById('city').innerText = cityName;
+    document.getElementById('temp').innerText = temp;
+    document.getElementById('des').innerText = description;
+  })
+  .catch(res => alert("Please type right city name"))
+})
